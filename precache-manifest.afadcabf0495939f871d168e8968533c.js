2a8b7efc17ebd8c5b9e61cbe907ff0c90e22350e
Loh Jia Hao <jia_hao_95@hotmail.com>
2019-09-01 00:35:21 +0800

    Updates

---
+++ precache-manifest.afadcabf0495939f871d168e8968533c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962a2e454d2ab27471a9fc05f580d66e",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "28ee09c9ab1dd2988c03",
+    "url": "/react-calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ccb00a07f36ce648904a",
+    "url": "/react-calculator/static/js/2.0f367be4.chunk.js"
+  },
+  {
+    "revision": "28ee09c9ab1dd2988c03",
+    "url": "/react-calculator/static/js/main.4388a5c6.chunk.js"
+  },
+  {
+    "revision": "2c7460d17796b892bd56",
+    "url": "/react-calculator/static/js/runtime~main.a948531e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
